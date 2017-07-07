7587bf8bd6a9a7af005335a188689c50cfc3c6e2
GrzegorzZapala <zapala.grzegorz@gmail.com>
2017-07-07 18:13:24 +0200

    start, stop i reset functions

--- test/test.js
+++ test/test.js
@@ -1,34 +1,71 @@
 
 
+//  całość znów bardziej przypomina jeden obiekt z metodami
 
 function tick () {
     let secs;
     let mins;
-    setInterval(()=> {
-        secs = Number(document.querySelector('#sec').textContent);
-        secs -= 1;
-        if (secs == -1) {
-            document.querySelector('#sec').textContent = secs = 59;
-            mins = Number(document.querySelector('#mins').textContent);
-            mins -= 1;
-            document.querySelector('#mins').textContent = mins;
-            if (mins == 0) {
-                // clear interval i
-                // wywołaj dźwięk i animację
-                return;
+    let intId;
+    //  = start();
+    //  = setInterval(() => {
+    //     secs = Number(document.querySelector('#sec').textContent);
+    //     secs -= 1;
+    //     if (secs == -1) {
+    //         document.querySelector('#sec').textContent = secs = 59;
+    //         mins = Number(document.querySelector('#mins').textContent);
+    //         mins -= 1;
+    //         document.querySelector('#mins').textContent = mins;
+    //         if (mins == 0) {
+    //             // clear interval i
+    //             // wywołaj dźwięk i animację
+    //             return;
+    //         }
+    //     }
+    //     document.querySelector('#sec').textContent = secs;
+    // }, 1000);
+    function clrInt () {
+        clearInterval(intId);
+    }
+    function reset () {
+        clrInt();
+        document.querySelector('#sec').textContent = 0;
+        document.querySelector('#sec').textContent = 0;
+    }
+    function start () {
+        intId = setInterval(function timer () {
+            secs = Number(document.querySelector('#sec').textContent);
+            console.log(secs);
+            secs -= 1;
+            if (secs == -1) {
+                document.querySelector('#sec').textContent = secs = 59;
+                mins = Number(document.querySelector('#mins').textContent);
+                mins -= 1;
+                document.querySelector('#mins').textContent = mins;
+                if (mins == 0) {
+                    // clear interval i
+                    // wywołaj dźwięk i animację
+                    return;
+                    // console.log('mins = 0');
+                }
             }
-        }
-        document.querySelector('#sec').textContent = secs;
-    }, 1000);
+            document.querySelector('#sec').textContent = secs;
+        }, 1000);
+    }
 
-    return secs;
+    document.querySelector('#stop').addEventListener('click', clrInt);
+    document.querySelector('#reset').addEventListener('click', reset);
+    document.querySelector('#start').addEventListener('click', start);
+
+        // czy po resecie musze removeEventListener wywoływać
+        // guzik reset by miał wywoływał clrInt oraz usuwał listener 
+        // listen'y bym usuwał wcześniej, przed przypisaniem.
+        // return secs;
 }
+tick();
 
 
+// QUnit.test('calculation test', function (assert) {
 
-
-QUnit.test('calculation test', function (assert) {
-
-    // priorytet działań
-    setTimeout(assert.deepEqual(tick(), 59), 1500); 
-});
+//     // priorytet działań
+//     setTimeout(assert.deepEqual(tick(), 59), 1500);
+// });
